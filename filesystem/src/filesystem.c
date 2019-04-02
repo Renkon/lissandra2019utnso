1b759594695159cffcb24b63d1faaf5dde82d61d
Renkon <fmbesteiro@gmail.com>
2019-04-01 23:55:33 -0300

    Mejoro la config. Lo inicial se hace en el thread principal

--- filesystem/src/filesystem.c
+++ filesystem/src/filesystem.c
@@ -8,21 +8,14 @@ int g_config_keys_size = 5;
 int main(void) {
 	g_logger = log_create("filesystem.log", "Filesystem", true, LOG_LEVEL_TRACE);
 
-	config_args_t* config_thread_args = build_config_args(g_config_keys, g_config_keys_size, FSCFG, g_logger, initialize_fs_config, update_fs_config);
-	pthread_t config_thread;
-
 	if (g_logger == NULL) {
 		// No pudimos crear el logger, asi que no podemos continuar
+		perror("No se pudo configurar logger. Abortando ejecucion");
 		return 1;
 	}
 
-	// Creo un hilo que se encarga de lo relacionado a configuracion
-	if (pthread_create(&config_thread, NULL, (void*) pre_init_config, (void*) config_thread_args)){
-		log_error(g_logger, "No se pudo inicializar el hilo de configuracion");
-		return 1;
-	}
+	init_config(FSCFG, initialize_fs_config, update_fs_config, g_config_keys, g_config_keys_size, g_logger);
 
-	pthread_join(config_thread, NULL);
 	log_destroy(g_logger);
 	return 0;
 }
