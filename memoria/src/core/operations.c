136dc4eadd1fa3ec781a4904788b3dfede907ec6
alvaromaser <alvaro199x@gmail.com>
2019-06-07 23:40:17 -0300

    Verificamos el tamaño maximo de lo que trae value en el insert

--- memoria/src/core/operations.c
+++ memoria/src/core/operations.c
@@ -57,39 +57,44 @@ void process_insert(insert_input_t* input) {
 	int index;
 	input->timestamp = get_timestamp();
 
-	found_segment = get_segment_by_name(g_segment_list, input->table_name);
+	if(strlen(input->value) <= 4){ //TODO Lo debemos traer de las configs del FS
 
-	if (found_segment != NULL) {
-		index_list = list_map(found_segment->page, (void*) page_get_index);
-		found_page = get_page_by_key(found_segment, index_list, input->key);
+		found_segment = get_segment_by_name(g_segment_list, input->table_name);
 
-		if (found_page != NULL) {
-			modify_memory_by_index(found_page->index, input->key, input->value);
-			found_page->modified = true;
+		if (found_segment != NULL) {
+			index_list = list_map(found_segment->page, (void*) page_get_index);
+			found_page = get_page_by_key(found_segment, index_list, input->key);
 
-			log_i("Se modifico el registro con key %u con el valor: %s ",input->key, input->value);
-		} else {
-			if (!memory_full()) {
-				index = memory_insert(input->timestamp, input->key, input->value);
-				found_page = create_page(index, true);
-				list_add(found_segment->page, found_page);
+			if (found_page != NULL) {
+				modify_memory_by_index(found_page->index, input->key, input->value);
+				found_page->modified = true;
 
-				log_i("Se inserto satisfactoriamente la clave %u con valor %s y timestamp %lld en la tabla %s", input->key, input->value, input->timestamp, found_segment->name);
+				log_i("Se modifico el registro con key %u con el valor: %s ",input->key, input->value);
 			} else {
-				//TODO JOURNALING + inserto devuelta.
+				if (!memory_full()) {
+					index = memory_insert(input->timestamp, input->key, input->value);
+					found_page = create_page(index, true);
+					list_add(found_segment->page, found_page);
+
+					log_i("Se inserto satisfactoriamente la clave %u con valor %s y timestamp %lld en la tabla %s", input->key, input->value, input->timestamp, found_segment->name);
+				} else {
+					//TODO JOURNALING + inserto devuelta.
+				}
 			}
-		}
 
-		list_destroy(index_list);
-	} else {
-		found_segment = create_segment(input->table_name);
-		index = memory_insert(input->timestamp, input->key, input->value);
-		found_page = create_page(index, true);
-		list_add(found_segment->page, found_page);
-		list_add(g_segment_list, found_segment);
+			list_destroy(index_list);
+		} else {
+			found_segment = create_segment(input->table_name);
+			index = memory_insert(input->timestamp, input->key, input->value);
+			found_page = create_page(index, true);
+			list_add(found_segment->page, found_page);
+			list_add(g_segment_list, found_segment);
 
-		log_i("Se inserto satisfactoriamente la clave %u con valor %s y timestamp %lld en la tabla %s recien creada", input->key, input->value, input->timestamp, found_segment->name);
- 	};
+			log_i("Se inserto satisfactoriamente la clave %u con valor %s y timestamp %lld en la tabla %s recien creada", input->key, input->value, input->timestamp, found_segment->name);
+		};
+	}else{
+		log_w("El tamaño del value es mayor al maximo disponible, cancelando operacion.");
+	}
 }
 
 void process_create(create_input_t* input) {
