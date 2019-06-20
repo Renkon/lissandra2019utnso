a70448458d00d935ef6ab228422848462c3dc190
Renkon <fmbesteiro@gmail.com>
2019-06-20 02:58:07 -0300

    Finalizo serializacion/deserializacion

--- lib/utils/serializer.c
+++ lib/utils/serializer.c
@@ -154,10 +154,17 @@ elements_network_t init_elements_info(int elements) {
 
 void serialize_content(void* to, socket_operation_t operation, void* from) {
 	select_input_t* select;
+	record_t* record_select;
 	insert_input_t* insert;
+	int* insert_response;
 	create_input_t* create;
+	int* create_response;
 	describe_input_t* describe;
+	t_list* metadata_describe;
+	table_metadata_t* metadata;
 	drop_input_t* drop;
+	int* drop_response;
+	int* journal_response;
 	char* to_ptr = (char*) to;
 	int offset = 0;
 	int elem_length;
@@ -172,6 +179,20 @@ void serialize_content(void* to, socket_operation_t operation, void* from) {
 
 			elem_length = sizeof(uint16_t);
 			memcpy(to_ptr + offset, &(select->key), elem_length);
+		break;
+		case SELECT_OUT:
+			record_select = (record_t*) from;
+
+			elem_length = sizeof(long long);
+			memcpy(to_ptr, &(record_select->timestamp), elem_length);
+			offset += elem_length;
+
+			elem_length = sizeof(int);
+			memcpy(to_ptr + offset, &(record_select->key), elem_length);
+			offset += elem_length;
+
+			elem_length = strlen(record_select->value) + 1;
+			memcpy(to_ptr + offset, record_select->value, elem_length);
 		break;
 		case INSERT_IN:
 			insert = (insert_input_t*) from;
@@ -191,6 +212,12 @@ void serialize_content(void* to, socket_operation_t operation, void* from) {
 			elem_length = sizeof(long long);
 			memcpy(to_ptr + offset, &(insert->timestamp), elem_length);
 		break;
+		case INSERT_OUT:
+			insert_response = (int*) from;
+
+			elem_length = sizeof(int);
+			memcpy(to_ptr, &insert_response, elem_length);
+		break;
 		case CREATE_IN:
 			create = (create_input_t*) from;
 
@@ -209,6 +236,12 @@ void serialize_content(void* to, socket_operation_t operation, void* from) {
 			elem_length = sizeof(long);
 			memcpy(to_ptr + offset, &(create->compaction_time), elem_length);
 		break;
+		case CREATE_OUT:
+			create_response = (int*) from;
+
+			elem_length = sizeof(int);
+			memcpy(to_ptr, &create_response, elem_length);
+		break;
 		case DESCRIBE_IN:
 			describe = (describe_input_t*) from;
 
@@ -217,10 +250,40 @@ void serialize_content(void* to, socket_operation_t operation, void* from) {
 				memcpy(to_ptr, describe->table_name, elem_length);
 			}
 		break;
+		case DESCRIBE_OUT:
+			metadata_describe = (t_list*) from;
+			for (int i = 0; i < list_size(metadata_describe); i++) {
+				metadata = (table_metadata_t*) list_get(metadata_describe, i);
+
+				elem_length = sizeof(consistency_t);
+				memcpy(to_ptr + offset, &(metadata->consistency), elem_length);
+				offset += elem_length;
+
+				elem_length = sizeof(int);
+				memcpy(to_ptr + offset, &(metadata->partitions), elem_length);
+				offset += elem_length;
+
+				elem_length = sizeof(long);
+				memcpy(to_ptr + offset, &(metadata->compaction_time), elem_length);
+				offset += elem_length;
+			}
+		break;
 		case DROP_IN:
 			drop = (drop_input_t*) from;
 			elem_length = strlen(drop->table_name) + 1;
 			memcpy(to_ptr, drop->table_name, elem_length);
+		break;
+		case DROP_OUT:
+			drop_response = (int*) from;
+
+			elem_length = sizeof(int);
+			memcpy(to_ptr, &drop_response, elem_length);
+		break;
+		case JOURNAL_OUT:
+			journal_response = (int*) from;
+
+			elem_length = sizeof(int);
+			memcpy(to_ptr, &journal_response, elem_length);
 		break;
 		default:
 		break;
@@ -229,10 +292,17 @@ void serialize_content(void* to, socket_operation_t operation, void* from) {
 
 void* deserialize_content(void* from, socket_operation_t operation, int elements, int* elements_size) {
 	select_input_t* select;
+	record_t* record_select;
 	insert_input_t* insert;
+	int* insert_response;
 	create_input_t* create;
+	int* create_response;
 	describe_input_t* describe;
+	t_list* metadata_describe;
+	table_metadata_t* metadata;
 	drop_input_t* drop;
+	int* drop_response;
+	int* journal_response;
 	int offset = 0;
 	int length;
 
@@ -246,6 +316,19 @@ void* deserialize_content(void* from, socket_operation_t operation, int elements
 			length = elements_size[1];
 			select->key = *((uint16_t*)(from + offset));
 			return select;
+		break;
+		case SELECT_OUT:
+			record_select = malloc(sizeof(record_t));
+			length = elements_size[0];
+			record_select->timestamp = *((long long*)(from + offset));
+			offset += length;
+			length = elements_size[1];
+			record_select->key = *((int*)(from + offset));
+			offset += length;
+			length = elements_size[2];
+			record_select->value = malloc(length);
+			memcpy(record_select->value, from + offset, length);
+			return record_select;
 		break;
 		case INSERT_IN:
 			insert = malloc(sizeof(insert_input_t));
@@ -264,6 +347,11 @@ void* deserialize_content(void* from, socket_operation_t operation, int elements
 			insert->timestamp = *((long long*)(from + offset));
 			return insert;
 		break;
+		case INSERT_OUT:
+			insert_response = malloc(sizeof(int));
+			memcpy(insert_response, from, elements_size[0]);
+			return insert_response;
+		break;
 		case CREATE_IN:
 			create = malloc(sizeof(create_input_t));
 			length = elements_size[0];
@@ -280,6 +368,11 @@ void* deserialize_content(void* from, socket_operation_t operation, int elements
 			create->compaction_time = *((long*)(from + offset));
 			return create;
 		break;
+		case CREATE_OUT:
+			create_response = malloc(sizeof(int));
+			memcpy(create_response, from, elements_size[0]);
+			return create_response;
+		break;
 		case DESCRIBE_IN:
 			describe = malloc(sizeof(describe_input_t));
 			if (elements == 0) {
@@ -290,11 +383,38 @@ void* deserialize_content(void* from, socket_operation_t operation, int elements
 			}
 			return describe;
 		break;
+		case DESCRIBE_OUT:
+			metadata_describe = list_create();
+			for (int i = 0; i < elements; i = i + 3) {
+				metadata = malloc(sizeof(table_metadata_t));
+				length = elements_size[i];
+				metadata->consistency = *((consistency_t*)(from + offset));
+				offset += length;
+				length = elements_size[i + 1];
+				metadata->partitions = *((int*)(from + offset));
+				offset += length;
+				length = elements_size[i + 2];
+				metadata->compaction_time = *((long*)(from + offset));
+				offset += length;
+				list_add(metadata_describe, metadata);
+			}
+			return metadata_describe;
+		break;
 		case DROP_IN:
 			drop = malloc(sizeof(drop_input_t));
 			drop->table_name = malloc(elements_size[0]);
 			memcpy(drop->table_name, from, elements_size[0]);
 			return drop;
+		break;
+		case DROP_OUT:
+			drop_response = malloc(sizeof(int));
+			memcpy(drop_response, from, elements_size[0]);
+			return drop_response;
+		break;
+		case JOURNAL_OUT:
+			journal_response = malloc(sizeof(int));
+			memcpy(journal_response, from, elements_size[0]);
+			return journal_response;
 		break;
 		default:
 		break;
