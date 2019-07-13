9b33cbddf81bfebb7ec5cee631deea545c0f0450
Renkon <fmbesteiro@gmail.com>
2019-07-13 08:51:35 -0300

    Un ultimo intento del compactador

--- filesystem/Debug/src/compactor/subdir.mk
+++ filesystem/Debug/src/compactor/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 src/compactor/%.o: ../src/compactor/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/git/tp-2019-1c-Papito-code-en-Assembler/lib" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -I"/home/utnso/Repositorios/tp-2019-1c-Papito-code-en-Assembler/lib" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
